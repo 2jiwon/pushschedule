718d144942d88245d4f866f6be2fbeb29632571b
ljw <ljw@e-no.tv>
2022-03-04 21:15:37 +0900

    import 부분 수정

--- main.go
+++ main.go
@@ -2,15 +2,17 @@ package main
 
 import (
 	"pushschedule/src/config"
-	scheduled "pushschedule/src/part"
+	"pushschedule/src/part"
 	"strconv"
 	"time"
 )
 
-func main() {
+func main() {	
 	scheduling_dtime, _ := strconv.Atoi(config.Get("SCHEDULING_CHECK_DELAY"))
 	for {
-		go scheduled.CheckScheduledPushData()
+		go part.CheckScheduledPushData()
+		go part.CheckRetargetQueueData()
+		go part.CheckPushAutoData()
 		time.Sleep(time.Minute * time.Duration(scheduling_dtime))
 	}
 }
