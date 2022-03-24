251b68bb088f0276f53dce98bae6e68b0913b450
ljw <ljw@e-no.tv>
2022-03-24 16:40:40 +0900

    스케쥴링 푸시 > 시간 계산 및 데이터 삽입 부분 수정

--- src/part/scheduled.go
+++ src/part/scheduled.go
@@ -16,10 +16,13 @@ import (
  */
  func CheckScheduledPushData() {
 	fmt.Println("체크 시작")
-	// KST로 timezone 설정
-
+	
 	now := time.Now()
+	// KST로 timezone 설정
 	now = now.In(time.FixedZone("KST", 9*60*60))
+	// time parse를 위한 location 지정
+	loc, _ := time.LoadLocation("Asia/Seoul")
+	formatted_now := now.Format("1504")
 
 	// 현재 홀수 주 인지 짝수 주인지 체크
 	_, week := now.ISOWeek()
@@ -28,13 +31,10 @@ import (
 	weekDay := int(now.Weekday())
 
 	// hhmm 형식으로 현재 시간 변환 (기준 시간은 5분)
-	now_timestamp := now.Unix()
 	schedule_time_limit := config.Get("SCHEDULING_LIMIT")
 	mins, _ := time.ParseDuration(schedule_time_limit + "m")
 	mins_limit := now.Add(-mins)
-	mins_timestamp := mins_limit.Unix()
-	hours, minutes, _ := mins_limit.Clock()
-	currentTime := fmt.Sprintf("%d%02d", hours, minutes)
+	formatted_mins := mins_limit.Format("1504")
 
 	// 스케쥴링 테이블
 	const tb_push_schedule_data = "BYAPPS2015_push_schedule_data"
@@ -71,15 +71,18 @@ import (
 				continue
 			}
 
-			// 5분 후로 설정한 시간과 일치하는지 체크
-			timely, _ := strconv.Atoi(mrow["timely"])
-			currTime, _ := strconv.Atoi(currentTime)
-			if timely != currTime {
+			// 제한 시간 내의 데이터인지 체크
+			if mrow["timely"] < formatted_mins || mrow["timely"] > formatted_now {
 				continue
 			}
 
+			// 스케쥴타임 넣기 위한 포맷변경
+			schedule_time_data := fmt.Sprintf("%s%s", now.Format("20060102"), mrow["timely"])
+			schedule_time, _ := time.ParseInLocation("200601021504", schedule_time_data, loc)
+
 			// push_msg_data에 데이터 삽입
 			f := map[string]interface{}{
+				"state" :        "A",
 				"app_id":        mrow["app_id"],
 				"push_type":     "schedule",
 				"msg_type":      mrow["msg_type"],
@@ -95,28 +98,34 @@ import (
 				"gcm_color":     mrow["gcm_color"],
 				"target_option": mrow["target_option"],
 				"fcm":           mrow["fcm"],
-				"schedule_time": strconv.FormatInt(mins_timestamp, 10),
-				"reg_time":      strconv.FormatInt(now_timestamp, 10),
-			}
-			if (mrow["app_os"] == "total") {
-				f["send_and"] = 1
-				f["send_ios"] = 1
-			} else if (mrow["app_os"] == "android") {
-				f["send_and"] = 1
-			} else {
-				f["send_ios"] = 1
+				"schedule_time": schedule_time.Unix(),
+				"reg_time": 	 now.Unix(),
 			}
 
-			res, res_idx := mysql.Insert("master", common.TB_push_msg_data, f, true)
-			if res < 1 {
-				helper.Log("error", "scheduled.CheckScheduledPushData", fmt.Sprintf("메시지 데이터 삽입 실패-%s", mrow))
-				common.SendJandiMsg("scheduled.CheckScheduledPushData", fmt.Sprintf("%s 메시지 전송 데이터 삽입 실패", mrow["app_id"]))
+			insert_res, res_idx := mysql.Insert("master", common.TB_push_msg_data, f, true)
+			if insert_res < 1 {
+				helper.Log("error", "scheduled.CheckScheduledPushData", fmt.Sprintf("push_msg_data Insert 실패-%s", mrow))
+				common.SendJandiMsg("scheduled.CheckScheduledPushData", fmt.Sprintf("push_msg_data Insert 실패-%s", mrow["app_id"]))
 			} else {
 				// push_msg_sends_ 에 데이터 삽입
-				go common.InsertPushMSGSendsData(res_idx, mrow["app_id"])
+				total_cnt, and_cnt, ios_cnt := common.InsertPushMSGSendsData(res_idx, mrow["app_id"])
+				if total_cnt == 0 {
+					helper.Log("error", "scheduled.CheckScheduledPushData", fmt.Sprintf("push_msg_sends_%s Insert된 내역이 없음", mrow["app_id"]))
+				} else {
+					// push_msg_data에 state와 발송수 업데이트
+					d := map[string]interface{} {
+						"state" : "R",
+						"send_and" : and_cnt,
+						"send_ios" : ios_cnt,
+					}
+					update_res := mysql.Update("master", common.TB_push_msg_data, d, "idx='" + strconv.Itoa(res_idx) + "'")
+					if update_res < 1 {
+						helper.Log("error", "scheduled.CheckScheduledPushData", fmt.Sprintf("push_msg_data Update 실패- idx : %d", res_idx))
+					}
+				}
 			}
 		}
 	} else {
-		helper.Log("error", "scheduled.CheckScheduledPushData", "수집된 데이터 없음")
+		helper.Log("error", "scheduled.CheckScheduledPushData", fmt.Sprintf("%s에서 수집된 데이터가 없음", tb_push_schedule_data))
 	}
 }
